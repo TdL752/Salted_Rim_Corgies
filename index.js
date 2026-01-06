163ae193aa99fb408bc7509aa2bb8d91541f4492
lint <tylerdamon1@aol.com>
2026-01-06 01:11:36 -0500

    added home route to heading text style changes to container

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/available', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'available.html'));
+});
+
+app.get('/contact', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 app.listen(port, () => {
 	console.log('listening on port 3000');
 });
